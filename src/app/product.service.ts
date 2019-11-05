a95dcd2c45ea87c979a05c08058361076f6c788b
Ifeanyichukwu Abraham Ossai <abraham.ossai@gmail.com>
2019-11-05 23:25:05 +0100

    add product-list component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,12 +2,14 @@ import 'rxjs/add/operator/map';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Album } from './album'
+import { Product } from './product'
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor( private _http: Http ) { }
 
@@ -15,6 +17,12 @@ export class ProductService {
     return this._http
             .get(this._albumUrl)
             .map(response => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http
+            .get(this._productsUrl)
+            .map(response => <Product[]>response.json());
   }
 
 }
